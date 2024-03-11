67f9057a706df60266129a226ae7f99d5bc79f3a
Julio Sanchez <jgs.sanchez94@gmail.com>
2024-03-11 17:18:47 -0300

    agregamos un puerto para correr el backend

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ import  express  from "express";
 console.log("hola mundo")
 
 //1- configurar puerto
-
+const app =express();
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), ()=>{
+    console.log("Servidor corriendo en el puerto: "+app.get("port"));
+});
 //2- definir los middlewares
+
+//
