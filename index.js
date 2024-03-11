ed13abf1b5ec9231c19500c58f93d11af5dd3a4a
Julio Sanchez <jgs.sanchez94@gmail.com>
2024-03-11 17:37:07 -0300

    agregamos cnfg de los middlewares

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import  express  from "express";
+import "dotenv/config";
+import cors from "cors";
+import morgan from "morgan";
 console.log("hola mundo")
 
 //1- configurar puerto
@@ -8,5 +11,8 @@ app.listen(app.get("port"), ()=>{
     console.log("Servidor corriendo en el puerto: "+app.get("port"));
 });
 //2- definir los middlewares
+app.use(cors()); //permite conexiones remotas
+app.use(morgan("dev")); //nos muestra info extra en la trminal
+app.use(express.json());//parsea los datos que vienen en formato json
+app.use(express.urlencoded({extended : true}));//para poder hacer peticiones de tipo POST y tener acceso a los datos deñ request
 
-//
