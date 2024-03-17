f250d42bf251e7076d603e1e2a8ba421434ebf18
Julio Sanchez <jgs.sanchez94@gmail.com>
2024-03-17 18:05:21 -0300

    agregamos el esquema de productos

---
+++ src/database/models/producto.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+
+const productoSchema = new Schema({
+  nombreProducto: {
+    type: String,
+    required: true,
+    minLength: 4,
+    maxlength: 50,
+    unique: true,
+  },
+  precio: { type: Number, required: true, min: 100, maxlength: 10000 },
+  descripcionProducto: {
+    type: String,
+    required: true,
+    minLength: 4,
+    maxlength: 50,
+  },
+  imagen: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (dato) => {
+        const pattern = /^(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)$/;
+        return pattern.test(dato);
+      },
+    },
+  },
+  descripcion_breve: {
+    type: String,
+    required: true,
+    minLength: 10,
+    maxlength: 50,
+  },
+  descripcion_amplia: {
+    type: String,
+    required: true,
+    minLength: 30,
+    maxlength: 300,
+  },
+});
