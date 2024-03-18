7811064fee640cb85e0d492c94ab3700d27e9695
Julio Sanchez <jgs.sanchez94@gmail.com>
2024-03-18 18:31:53 -0300

    agregamos la logica para editar productos

--- src/controllers/productos.controller.js
+++ src/controllers/productos.controller.js
@@ -49,3 +49,24 @@ export const crearProducto= async(req,res)=>{
         })
     }
 }
+export const editarProductos = async(req,res)=>{
+    try {
+       const productoBuscado = await Producto.findById(req.params.id)
+       if (!productoBuscado){
+        return res.status(404).json({
+           mensaje : 'No se ha encontrado un producto con ese ID'
+         });   
+       }
+       await Producto.findByIdAndUpdate(req.params.id, req.body)
+       res.status(200).json({mensaje:"el producto due editado correctamente"});
+
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            mensaje: "error al editar el producto"
+        })
+    }
+
+
+};
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { ListarProductos, crearProducto, obtenerProductos } from "../controllers/productos.controller.js";
+import { ListarProductos, crearProducto, editarProductos, obtenerProductos } from "../controllers/productos.controller.js";
 
 const router = Router();
 router.route("/productos").get(ListarProductos).post(crearProducto);
-router.route("/productos/:id").get(obtenerProductos)
+router.route("/productos/:id").get(obtenerProductos).put(editarProductos).delete(eliminarProducto)
 export default router;
